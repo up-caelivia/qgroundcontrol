253f7a1ba04ed80d5aa97159aca14e04c38c38a7
ivanbrillo <brilloivan2@gmail.com>
2024-07-02 16:29:46 +0200

    last fixes for compile reasons

--- src/Settings/VideoSettings.cc
+++ src/Settings/VideoSettings.cc
@@ -128,6 +128,9 @@ DECLARE_SETTINGSFACT(VideoSettings, streamEnabled)
 DECLARE_SETTINGSFACT(VideoSettings, disableWhenDisarmed)
 DECLARE_SETTINGSFACT(VideoSettings, lowLatencyMode)
 
+// Herelink HDMI source switching
+DECLARE_SETTINGSFACT(VideoSettings, cameraId)
+
 DECLARE_SETTINGSFACT_NO_FUNC(VideoSettings, videoSource)
 {
     if (!_videoSourceFact) {
