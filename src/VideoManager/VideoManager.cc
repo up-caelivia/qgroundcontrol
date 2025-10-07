8471d4d11a840623d69d7e8b06afe574f5a0e54e
Adriano Della Frera <adriano.dellafrera@up-caelivia.it>
2025-10-07 13:02:11 +0200

    fix(video): fixed video rstp address

--- src/VideoManager/VideoManager.cc
+++ src/VideoManager/VideoManager.cc
@@ -767,7 +767,7 @@ VideoManager::_updateSettings(unsigned id)
     else if (source == VideoSettings::videoSourceSkydroidH12)
         settingsChanged |= _updateVideoUri(0, QStringLiteral("rtsp://192.168.144.108:554/stream=0"));
     else if (source == VideoSettings::videoSourceSkydroidH16)
-        settingsChanged |= _updateVideoUri(0, QStringLiteral("rtsp://192.168.0.10:8554/H264Video"));
+        settingsChanged |= _updateVideoUri(0, QStringLiteral("rtsp://192.168.43.1:8554/fpv_stream"));
     else if (source == VideoSettings::videoDisabled || source == VideoSettings::videoSourceNoVideo)
         settingsChanged |= _updateVideoUri(0, "");
     else {
