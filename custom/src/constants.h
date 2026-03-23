4849af45b70e6f48d796d47310bf2293d2f2d490
Adriano Della Frera <adriano.dellafrera@flyingbasket.com>
2026-03-23 17:14:08 +0100

    added  LOIT_ACC_MAX to speeds mode

--- custom/src/constants.h
+++ custom/src/constants.h
@@ -115,11 +115,10 @@ public:
         }
     }
 
-    QVector<QString> factSpeedNames() const { return {"LOIT_SPEED", "PILOT_SPEED_DN", "PILOT_SPEED_UP", "PILOT_Y_RATE"}; }
-    QVector<int> lowSpeed() const { return {260, 50, 100, 30}; }
-    QVector<int> normalSpeed() const { return {500, 100, 150, 45}; }
-    //QVector<int> highSpeed() const { return {1000, 250, 500, 60}; }   // CNES
-    QVector<int> highSpeed() const { return {1000, 150, 250, 60}; }
+    QVector<QString> factSpeedNames() const { return {"LOIT_SPEED", "PILOT_SPEED_DN", "PILOT_SPEED_UP", "PILOT_Y_RATE", "LOIT_ACC_MAX"}; }
+    QVector<int> lowSpeed() const { return {260, 50, 100, 30, 200}; }
+    QVector<int> normalSpeed() const { return {500, 100, 150, 45, 400}; }
+    QVector<int> highSpeed() const { return {1000, 150, 250, 60, 400}; }
 
     //Non superare i 19 m/s per certificazione
 
