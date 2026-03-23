8af23c7f3b3124d56842bb3e6bef0ee39759fa00
Adriano Della Frera <adriano.dellafrera@flyingbasket.com>
2026-03-23 17:14:43 +0100

    updated LOIT_ACC_MAX in slow speed mode

--- custom/src/constants.h
+++ custom/src/constants.h
@@ -116,7 +116,7 @@ public:
     }
 
     QVector<QString> factSpeedNames() const { return {"LOIT_SPEED", "PILOT_SPEED_DN", "PILOT_SPEED_UP", "PILOT_Y_RATE", "LOIT_ACC_MAX"}; }
-    QVector<int> lowSpeed() const { return {260, 50, 100, 30, 200}; }
+    QVector<int> lowSpeed() const { return {250, 50, 100, 30, 200}; }
     QVector<int> normalSpeed() const { return {500, 100, 150, 45, 400}; }
     QVector<int> highSpeed() const { return {1000, 150, 250, 60, 400}; }
 
