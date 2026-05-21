b81b9f7b0b157229a5dc7b50c903e4dbb1296827
Adriano Della Frera <adriano.dellafrera@flyingbasket.com>
2026-05-21 23:58:53 +0200

    reverted geoawareness horizontal buffer for warning to 5s without 20% margin

--- custom/src/GeoAwareness/KmlPolygonLoader.cpp
+++ custom/src/GeoAwareness/KmlPolygonLoader.cpp
@@ -974,8 +974,7 @@ bool KmlPolygonLoader::checkDronePosition()
     // Note: ArduPilot params are usually in cm/s -> convert to m/s
     // --------------------------------------------------------
 
-    const double bufferSecHor = 5.5;
-    const double bufferSecVert = 5.0;
+    const double bufferSec = 5;
 
     auto* pm = vehicle->parameterManager();
 
@@ -998,11 +997,8 @@ bool KmlPolygonLoader::checkDronePosition()
     const double loitSpeed_m_s    = loitSpeed_cm_s / 100.0;
     const double pilotUp_m_s      = pilotSpeedUp_cm_s / 100.0;
 
-    double nearHorizMeters = bufferSecHor * loitSpeed_m_s;
-    if (loitSpeed_m_s > 6) {
-        nearHorizMeters = nearHorizMeters*1.2; // 20% extra buffer
-    }
-    double nearVertMeters  = bufferSecVert * pilotUp_m_s;
+    double nearHorizMeters = bufferSec * loitSpeed_m_s;
+    double nearVertMeters  = bufferSec * pilotUp_m_s;
     if (pilotUp_m_s > 1.5) {
         nearVertMeters = nearVertMeters*1.2; // 20% extra buffer
     }
